666c64cf06a70eeb249320ec6ffb3b0d481c4bef
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-06-25 13:14:31 +0200

    typeclass code imported.

---
+++ cpp_typeclass/code/hdr/typeclass.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <list>
+#include <string>
+
+#include "type-multiset.hpp"
+
+template <typename ...Fs>
+using typeclass = type::multiset<Fs...>;
+
+template <template <typename> class Cl, typename Ty, typename ...Fs>
+struct __typeclass_instance
+{
+    static_assert( type::equal_set< type::multiset<Fs...>, typename Cl<Ty>::type >(), "TypeClass: instance error");
+
+    using type = typeclass<Fs...>;
+};
+
+template <template <typename> class Cl, typename Ty, typename ...Fs>
+using typeclass_instance = typename __typeclass_instance<Cl, Ty, Fs...>::type;
+
+template <template <typename> class Cl, typename Ty, typename ...Fs>
+struct is_instance_of : std::integral_constant<bool, true>
+{
+    using type = typename Cl<Ty>::type;
+};
+
+#define has_function_(fun) \
+template <typename Ty> \
+struct has_function_ ## fun \
+{ \
+    using yes = char[1]; \
+    using no  = char[2]; \
+    \
+    template <typename V, V value> struct type_check; \
+    template <typename U> static yes& sfinae(type_check<decltype(fun),&fun> *); \
+    template <typename U> static no&  sfinae(...); \
+    \
+    static constexpr bool value = sizeof(sfinae<Ty>(0)) == sizeof(yes); \
+};
+
+
+
---
+++ cpp_typeclass/code/typeclass.cpp
@@ -0,0 +1,75 @@
+#include <list>
+#include <string>
+#include <iostream>
+
+#include "hdr/typeclass.hpp"
+
+
+///////////////// Show typeclass (simplified)
+
+
+template <typename T>
+struct Show
+{
+    static std::string show(T const &);
+    static std::string showList(std::list<T> const &);
+
+    using type = typeclass
+                 <
+                   decltype(show),
+                   decltype(showList)
+                 >;
+
+    has_function_(show);
+    has_function_(showList);
+};
+
+
+template <typename Ty>
+constexpr bool ShowInstance()
+{
+    return  Show<Ty>::template has_function_show<Ty>::value &&
+            Show<Ty>::template has_function_showList<std::list<Ty>>::value;
+};
+
+
+///////////////// User defined type:
+
+
+struct Test { };
+
+std::string show(Test const &)
+{
+    return "test";
+}
+
+std::string showList(std::list<Test> const &)
+{
+    return "[test]";
+}
+
+
+//// ensure the interface is correct:
+
+
+using Show_Test_instance =
+
+    typeclass_instance
+    <
+        Show, Test,
+        decltype(showList),
+        decltype(show)
+    >;
+
+
+int
+main(int, char *[])
+{
+     static_assert(ShowInstance<Test>(), "Show instance error");
+
+     std::cout << show(Test{}) << std::endl;
+     std::cout << showList(std::list<Test>{}) << std::endl;
+
+     return 0;
+}
+
